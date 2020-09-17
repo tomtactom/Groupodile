0eb3170c164c3855d090b071d9cf02a92389d6db
12tom12 <32821177+12tom12@users.noreply.github.com>
2020-09-17 19:24:29 +0200

    bugfix

--- setup/templates/websitesettings.inc.php
+++ setup/templates/websitesettings.inc.php
@@ -12,7 +12,7 @@
 				}
 			</style>
 				<label for="inputSiteurl">Website URL:
-					<input type="url" id="inputSiteurl" name="siteurl" minlength="12" maxlength="255" value="<?php echo $_SERVER['HTTP_REFERER'].explode('/setup/')[0]; ?>" required>
+					<input type="url" id="inputSiteurl" name="siteurl" minlength="12" maxlength="255" value="<?php echo str_replace("/setup/", "", $_SERVER['HTTP_REFERER']); ?>" required>
 				</label>
 				<label for="inputSitename">Website Name:
 					<input type="text" id="inputSitename" name="sitename" maxlength="255" value="Groupodile" required>
