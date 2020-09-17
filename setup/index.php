998810b371b5d9d365a81758a9a729d33dbf3891
12tom12 <32821177+12tom12@users.noreply.github.com>
2020-09-17 19:48:31 +0200

    Database file Bugfix

--- setup/index.php
+++ setup/index.php
@@ -5,18 +5,22 @@
 		header("Location: ../setup");
 	}
 
+	if(!file_exists("include/database/database.php")) {
+		file_put_contents('include/database/database.php', '');
+	}
+
 	//Zeige nächte schritte
 	$nextStep = "introduction";
 	if (isset($_POST['nextStep']))
 		$nextStep = $_POST['nextStep'];
-	
+
 	// Definiere Variablen
 	$step = $nextStep;
 	$title = $step;
 	$header = $config['header'];
 	$product = $introduction["product"];
 	$setup = true;
-	
+
 	include("templates/head.inc.php");
 	include("templates/header.inc.php");
 	include($nextStep.".inc.php");
